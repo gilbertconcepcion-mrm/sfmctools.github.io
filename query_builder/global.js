a827bfc3e30f98dbdd571c14c86ac5f15975743a
gilbertconcepcion-mrm <gilbert.concepcion@mrm.com>
2025-10-29 16:56:21 +0800

    Updates
    Updates

---
+++ query_builder/global.js
@@ -0,0 +1,19 @@
+// Update config object
+config = {
+    tier: {
+        mac: { key1: "A-List Classic", key2: "MAC", checked: "" },
+        mea: { key1: "A-List", key2: "MEA", checked: "" },
+        mei: { key1: "Insider", key2: "MEI", checked: "" },
+        mep: { key1: "Premiere", key2: "MEP", checked: "" },
+        mpg: { key1: "PremiereGo", key2: "MPG", checked: "" }
+    },
+    genre: "NONE",
+    member: { membership: "Member", activeStatus: "Active", age: "13" },
+    query: { split: "1", dev1: "", dev2: "", sendlist: "", additionalFields: "",sendlistDataField: "EmailAddress",joinFieldSubscriber: "EmailAddress" },
+    optIn: "None Selected",
+    amcMasterSupression: "",
+    associateSupression: "",
+    freshAddressExclude: "",
+    engagement: "",
+    complaintsRemoval: ""
+};
